0da90cc8f8a4f7ee4b7412cfc6aacc0a8c67a673
Jono <work@jrkd.co.uk>
2021-06-11 15:09:25 +1200

    both demos - fixed issues around launch and relative js path in html
    goal-demo
    - added ability to set current state and goal state of world
    - now lists out actions
    - can run search multiple times
    - can now debug properly

--- goal-demo/src/index.ts
+++ goal-demo/src/index.ts
@@ -10,18 +10,55 @@ import { IGraphEdge } from "../../lib/output/graphedge";
 /* global Graph, astar, $ */
 
 
-var WALL = 10000,
-    performance = window.performance;
-
 $(function() {
+    $(".run-search").click(()=>{
+        runSearch();
+    });
+});
+
+function runSearch() {
     var $plannerResults = $("#plannerList");
+    $plannerResults.empty();
     var planner = new Planner();
 
     //setup the actions we can use. 
     //planner.nodes -- we dont really have a full list of them right? cause each action just tweaks them?...
     //suppose you could make things quick by pre-processing?
     //actions:
-    let moveToBank:NodeAction = new NodeAction();
+    let actions: NodeAction[] = setupActions();
+    renderActions(actions);
+
+    planner.actions = actions;
+
+    //setup current state
+    let startState: WorldState = new WorldState();
+    $.extend(startState, retrieveWorldState($(".initial-worldstate"))); //new WorldState();
+
+    let goalState: WorldState = new WorldState();
+    $.extend(goalState, retrieveWorldState($(".goal-worldstate")));
+
+    let startNode: GoalNode = new GoalNode();
+    startNode.state = startState;
+    let goalNode: GoalNode = new GoalNode();
+    goalNode.state = goalState;
+
+    planner.preprocessGraph(startNode);
+
+    let results = AStar.search(planner, startNode, goalNode, {});
+
+    let currentNode: GoalNode = startNode;
+
+    if (results.length == 0) {
+        $plannerResults.html("<h1>no plan available!</h1>");
+    }
+    results.forEach((result: GoalEdge) => {
+        currentNode = result.action.ActivateAction(currentNode);
+        $plannerResults.append("<li>" + result.action.name + "</li>");
+    });
+}
+
+function setupActions():NodeAction[] {
+    let moveToBank: NodeAction = new NodeAction();
     moveToBank.name = "Move to bank";
     moveToBank.cost = 5;
     moveToBank.preconditions = new WorldState();
@@ -31,7 +68,7 @@ $(function() {
     moveToBank.effects.myPosX = 100;
     moveToBank.effects.myPosY = 100;
 
-    let takeMoneyFromBank:NodeAction = new NodeAction();
+    let takeMoneyFromBank: NodeAction = new NodeAction();
     takeMoneyFromBank.name = "Take money from bank";
     takeMoneyFromBank.cost = 1;
     takeMoneyFromBank.preconditions = new WorldState();
@@ -42,7 +79,15 @@ $(function() {
     takeMoneyFromBank.effects.moneyWithMe = 100;
     takeMoneyFromBank.effects.moneyAtBank = 0;
 
-    let buyPizza:NodeAction = new NodeAction();
+    let workAJob: NodeAction = new NodeAction();
+    workAJob.name = "Another day, another dolla (Work)";
+    workAJob.cost = 1;
+    workAJob.preconditions = new WorldState();
+    workAJob.effects = new WorldState();
+    workAJob.effects.moneyAtBank = 100;
+    
+
+    let buyPizza: NodeAction = new NodeAction();
     buyPizza.name = "Buy pizza";
     buyPizza.cost = 1;
     buyPizza.preconditions = new WorldState();
@@ -52,7 +97,7 @@ $(function() {
     buyPizza.effects.moneyWithMe = 0;
     buyPizza.effects.hungry = false;
 
-    let makeToastie:NodeAction = new NodeAction();
+    let makeToastie: NodeAction = new NodeAction();
     makeToastie.name = "Make Toastie";
     makeToastie.cost = 4;
     makeToastie.preconditions = new WorldState();
@@ -62,59 +107,40 @@ $(function() {
     makeToastie.effects = new WorldState();
     makeToastie.effects.hungry = false;
 
-    let getSliceOfBread:NodeAction = new NodeAction();
+    let getSliceOfBread: NodeAction = new NodeAction();
     getSliceOfBread.name = "Get Slice of Bread";
     getSliceOfBread.cost = 1;
     getSliceOfBread.preconditions = new WorldState();
     getSliceOfBread.effects = new WorldState();
     getSliceOfBread.effects.numBread = 2;
 
-    let getCheese:NodeAction = new NodeAction();
+    let getCheese: NodeAction = new NodeAction();
     getCheese.name = "Get cheese";
     getCheese.cost = 1;
     getCheese.preconditions = new WorldState();
     getCheese.effects = new WorldState();
     getCheese.effects.numCheese = 1;
+    return [ moveToBank, buyPizza, workAJob, takeMoneyFromBank, makeToastie, getSliceOfBread, getCheese ];
+}
 
-    planner.actions = [moveToBank, buyPizza, takeMoneyFromBank, makeToastie, getSliceOfBread, getCheese];
+function renderActions(actions:NodeAction[]){
+    const $actionList = $(".action-list");
 
-    //setup current state
-    let startState:WorldState = new WorldState();
-    startState.hungry = true;
-    startState.moneyWithMe = 0;
-    startState.numFoodRecipes = 1;
-    startState.moneyAtBank = 100;
-    startState.bankPosX = 100;
-    startState.bankPosY = 100;
-    startState.numBread = 0;
-    startState.numCheese = 0;
-    startState.myPosX = 0;
-    startState.myPosY = 0;
+    let resultList = "";
+    actions.forEach((action:NodeAction) => {
+        let actionHtml = $(".action-item-template").html();
+        actionHtml = actionHtml.replace("{{name}}", action.name);
+        actionHtml = actionHtml.replace("{{cost}}", action.cost.toString());
+        actionHtml = actionHtml.replace("{{preconditions}}", JSON.stringify( action.preconditions ) );
+        actionHtml = actionHtml.replace("{{effects}}", JSON.stringify( action.effects ) );
 
-    let goalState:WorldState = new WorldState();
-    goalState.hungry = false;
-
-    let startNode:GoalNode = new GoalNode();
-    startNode.state = startState;
-    let goalNode:GoalNode = new GoalNode();
-    goalNode.state = goalState;
-
-    planner.preprocessGraph(startNode);
-
-    let results = AStar.search(planner, startNode, goalNode, {});
-  
-    let currentNode:GoalNode = startNode;
-
-    if(results.length == 0){
-        $plannerResults.html("<h1>no plan available!</h1>");
-    }
-    results.forEach((result:GoalEdge) => {
-        currentNode = result.action.ActivateAction(currentNode);
-        $plannerResults.append("<li>"+result.action.name+"</li>");
+        resultList += actionHtml;
     });
+    $actionList.empty();
+    $actionList.append(resultList);
+}
 
-    $("#btnGenerate").click(function() {
-        //grid.initialize();
-    });
-
-});
+function retrieveWorldState($element):WorldState{
+    let jsonObj:WorldState = JSON.parse($element.val());
+    return jsonObj;
+}
